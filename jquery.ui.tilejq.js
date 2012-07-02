3119ba886cfad73e576d3cc9e10c2d0f0a0da5ce
Curtis Schlak <fuzzygeek1@users.sourceforge.net>
2012-07-01 21:25:10 -0500

    Handle icon clicks differently. Added minimizer. Rid myself of dblclick handlers. I don't like dblclicks.

--- jquery.ui.tilejq.js
+++ jquery.ui.tilejq.js
@@ -7,25 +7,6 @@
       var tilejq = this
         , firstTile = tilejq.element.children().first()
         , firstTileSmall = firstTile.children().first()
-        , maximizeTile = function(e) {
-            e.stopPropagation();
-            tilejq.tools
-              .hide();
-            var tileToMove = $(e.target)
-              .closest('.ui-tilejq-tile')
-              .addClass('ui-tilejq-static')
-              .width('')
-              .height('');
-            var tileToReturn = tilejq.max
-              .children()
-              .removeClass('ui-tilejq-static')
-              .height(tilejq._smHeight)
-              .width(tilejq._smWidth)
-              .remove();
-            tilejq.element.append(tileToReturn);
-            tilejq.max.append(tileToMove);
-            tilejq.min.width(tilejq._smWidth + 32);
-          }
         ;
       tilejq._width = firstTile.width();
       tilejq._height = firstTile.outerHeight();
@@ -33,43 +14,62 @@
       tilejq._smHeight = firstTileSmall.height();
 
       tilejq.wrapper = $('<div class="ui-widget ui-tilejq"><div class="ui-tilejq-max"></div><div class="ui-tilejq-min"></div></div>');
-      tilejq.tools = $('<div class="ui-tilejq-tools ui-widget ui-widget-content"><div class="ui-icon ui-icon-arrow-4-diag"></div></div>')
-        .hide()
-        .on('click', '.ui-icon', function(e) {
-          e.stopPropagation();
-          maximizeTile({
-            stopPropagation: function() {},
-            target: this
-          });
-        })
-        .on('dblclick', '.ui-icon', function(e) {
-          e.stopPropagation();
-        });
+      tilejq.maximizer = $('<div class="ui-tilejq-tools ui-widget ui-widget-content"><div class="ui-icon ui-icon-arrow-4-diag"></div></div>')
+        .hide();
+      tilejq.minimizer = $('<div class="ui-tilejq-tools ui-widget ui-widget-content"><div class="ui-icon ui-icon-arrowthick-1-sw"></div></div>')
+        .hide();
       tilejq.max = tilejq.wrapper
         .children()
         .first()
-          .on('dblclick', function(e) {
-            var tileToReturn = $(this)
-              .children()
-              .removeClass('ui-tilejq-static')
-              .height(tilejq._smHeight)
-              .width(tilejq._smWidth)
-              .remove();
-            tilejq.element.append(tileToReturn);
-            tilejq.min.width('');
-            tilejq.element.height('');
-          });
+        .on('mouseover', '.ui-tilejq-tile-header', function(e) {
+          e.stopPropagation();
+          tilejq.minimizer
+            .appendTo($(this))
+            .width('')
+            .show();
+        })
+        .on('click', '.ui-icon', function(e) {
+          e.stopPropagation();
+          tilejq.minimizer.hide();
+          var tileToReturn = tilejq.max
+            .children()
+            .removeClass('ui-tilejq-static')
+            .height(tilejq._smHeight)
+            .width(tilejq._smWidth)
+            .remove();
+          tilejq.element.append(tileToReturn);
+          tilejq.min.width('');
+          tilejq.element.height('');
+        });
       tilejq.min = tilejq.wrapper.children().last();
       tilejq.element
         .sortable()
         .addClass('ui-tilejq-min-scroller')
-        .on('dblclick', maximizeTile)
         .on('mouseover', '.ui-tilejq-tile-header', function(e) {
           e.stopPropagation();
-          tilejq.tools
+          tilejq.maximizer
             .appendTo($(this))
             .width('')
             .show();
+        })
+        .on('click', '.ui-icon', function(e) {
+          e.stopPropagation();
+          tilejq.maximizer
+            .hide();
+          var tileToMove = $(e.target)
+            .closest('.ui-tilejq-tile')
+            .addClass('ui-tilejq-static')
+            .width('')
+            .height('');
+          var tileToReturn = tilejq.max
+            .children()
+            .removeClass('ui-tilejq-static')
+            .height(tilejq._smHeight)
+            .width(tilejq._smWidth)
+            .remove();
+          tilejq.element.append(tileToReturn);
+          tilejq.max.append(tileToMove);
+          tilejq.min.width(tilejq._smWidth + 32);
         })
         .children()
           .addClass('ui-widget ui-tilejq-tile')
@@ -89,7 +89,8 @@
           .height(tilejq._smHeight)
           .width(tilejq._smWidth);
       $(document).on('mouseover', function() {
-        tilejq.tools.hide();
+        tilejq.maximizer.hide();
+        tilejq.minimizer.hide();
       });
     }
   });

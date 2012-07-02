e3438636fa02fb9ac65a7966b2d078e2216a6720
Curtis Schlak <fuzzygeek1@users.sourceforge.net>
2012-07-01 21:00:16 -0500

    Added maximizer tool.

--- jquery.ui.tilejq.js
+++ jquery.ui.tilejq.js
@@ -7,6 +7,25 @@
       var tilejq = this
         , firstTile = tilejq.element.children().first()
         , firstTileSmall = firstTile.children().first()
+        , maximizeTile = function(e) {
+            e.stopPropagation();
+            tilejq.tools
+              .hide();
+            var tileToMove = $(e.target)
+              .closest('.ui-tilejq-tile')
+              .addClass('ui-tilejq-static')
+              .width('')
+              .height('');
+            var tileToReturn = tilejq.max
+              .children()
+              .removeClass('ui-tilejq-static')
+              .height(tilejq._smHeight)
+              .width(tilejq._smWidth)
+              .remove();
+            tilejq.element.append(tileToReturn);
+            tilejq.max.append(tileToMove);
+            tilejq.min.width(tilejq._smWidth + 32);
+          }
         ;
       tilejq._width = firstTile.width();
       tilejq._height = firstTile.outerHeight();
@@ -14,43 +33,49 @@
       tilejq._smHeight = firstTileSmall.height();
 
       tilejq.wrapper = $('<div class="ui-widget ui-tilejq"><div class="ui-tilejq-max"></div><div class="ui-tilejq-min"></div></div>');
+      tilejq.tools = $('<div class="ui-tilejq-tools ui-widget ui-widget-content"><div class="ui-icon ui-icon-arrow-4-diag"></div></div>')
+        .hide()
+        .on('click', '.ui-icon', function(e) {
+          e.stopPropagation();
+          maximizeTile({
+            stopPropagation: function() {},
+            target: this
+          });
+        })
+        .on('dblclick', '.ui-icon', function(e) {
+          e.stopPropagation();
+        });
       tilejq.max = tilejq.wrapper
         .children()
         .first()
-        .on('dblclick', function(e) {
-          var tileToReturn = $(this)
-            .children()
-            .removeClass('ui-tilejq-static')
-            .height(tilejq._smHeight)
-            .width(tilejq._smWidth)
-            .remove();
-          tilejq.element.append(tileToReturn);
-          tilejq.min.width('');
-          tilejq.element.height('');
-        });
+          .on('dblclick', function(e) {
+            var tileToReturn = $(this)
+              .children()
+              .removeClass('ui-tilejq-static')
+              .height(tilejq._smHeight)
+              .width(tilejq._smWidth)
+              .remove();
+            tilejq.element.append(tileToReturn);
+            tilejq.min.width('');
+            tilejq.element.height('');
+          });
       tilejq.min = tilejq.wrapper.children().last();
       tilejq.element
         .sortable()
         .addClass('ui-tilejq-min-scroller')
-        .on('dblclick', function(e) {
-          var tileToMove = $(e.target)
-            .closest('.ui-tilejq-tile')
-            .addClass('ui-tilejq-static')
+        .on('dblclick', maximizeTile)
+        .on('mouseover', '.ui-tilejq-tile-header', function(e) {
+          e.stopPropagation();
+          tilejq.tools
+            .appendTo($(this))
             .width('')
-            .height('');
-          var tileToReturn = tilejq.max
-            .children()
-            .removeClass('ui-tilejq-static')
-            .height(tilejq._smHeight)
-            .width(tilejq._smWidth)
-            .remove();
-          tilejq.element.append(tileToReturn);
-          tilejq.max.append(tileToMove);
-          tilejq.min.width(tilejq._smWidth + 32);
-          tilejq.element.height(tilejq.min.outerHeight());
+            .show();
         })
         .children()
           .addClass('ui-widget ui-tilejq-tile')
+          .children(':first-child')
+            .addClass('ui-tilejq-tile-header')
+          .end()
           .children(':last-child')
             .addClass('ui-widget-content')
           .end()
@@ -63,6 +88,9 @@
         .children()
           .height(tilejq._smHeight)
           .width(tilejq._smWidth);
+      $(document).on('mouseover', function() {
+        tilejq.tools.hide();
+      });
     }
   });
 })(jQuery);
